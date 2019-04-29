2414c30eca873c21bee001dd15279851d073664b
teastonsmith2 <t.easton.smith2@gmail.com>
2019-04-29 16:51:21 -0600

    end of class commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  constructor() {
-    super()
+// class App extends Component {
+function App(props) {
+	constructor() {
+		super();
 
-    this.state = {
-      friends: [],
-      picture: [],
-      name: ''
-    }
+		this.state = {
+			friends: [],
+			picture: '',
+			name: '',
+		};
+	}
 
-    updatePicture((value) => {
-    this.setState.picture
-    })
+	updatePicture(value) {
+		this.picture.setState(value);
+		// this.setState({ picture: value });
+	}
 
-    updateName((value) => {
-      this.setState.name
-    })
+	updateName(value) {
+		this.name.setState(value);
+		// this.setState({ name: value })
+	}
 
-    addFriend(() => {
-      this.setState.push(friends)
-      this.setState
-    })
-  }
+	addFriend() {
+		// const { friends, picture, name } = this.state
+		// let newFriends = friends.slice()
+		// newFriends.push({picture, name})
+		// this.setState({ friends: newFriends, picture: '', name: ''})
 
-  render() {
+		this.setState.push(friends);
+		friends.push(this.setState);
+		this.setState;
+	}
+``
+	render() {
     const friends = this.state.friends.map(() => {
     return (
-      <div>
-        <img src={this.friends.picture} height="100px" width="100px"></img>
-          <span>{this.name}</span>
-      </div>
-      )
-    })
-    return (
-      <div key="parent">
-        <input onChange={this.state.updatePicture()}>
-          <span>Enter Picture URL</span>
-        </input>
-        <input onChange={this.state.updateName()}>
-          <span>Enter Name</span>
-        </input>
-        <button onClick={this.state.addFriend}>Add Friend</button>
-        <div>{this.friends}</div>
-      </div>
-    );
-  }
+				<div key='parent1'>
+					<img src={this.friends.picture} height='100px' width='100px' />
+					<span>{this.friends.name}</span>
+				</div>
+			);
+		});
+		return (
+			<div key='parent2'>
+				<span>Enter Picture URL</span>
+				<input onChange={this.state.updatePicture(e.target.value)} />
+				{/* <input onChange={ ( e ) => this.updatePicture( e.target.value ) } value={ this.state.picture } /> */}
+
+				<span>Enter Name</span>
+				<input onChange={this.state.updateName(e.target.value)} />
+				{/* <input onChange={ ( e ) => this.updateName( e.target.value ) } value={ this.state.name } /> */}
+
+				<button
+					// onClick={ () => this.addFriend() }
+					onClick={addFriend(() => {
+						this.setState();
+					})}>
+					Add Friend
+				</button>
+
+				<div>{this.state.friends}</div>
+			</div>
+		);
+	}
 }
 
 export default App;
